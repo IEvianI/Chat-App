10244a46b87bb09b1cc8bd37883ab67e463e5cd2
Evian <allainevan1@gmail.com>
2024-08-21 12:28:03 +0200

    Trad login fr

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -81,25 +81,25 @@ const Login = () => {
     return(
         <div className="login">
             <div className="item">
-                <h2>Welcome back,</h2>
+                <h2>Bon retour,</h2>
                 <form onSubmit={handleLogin}>
                     <input type="text" placeholder='Email' name='email'/>
-                    <input type="password" placeholder='Password' name='password'/>
-                    <button disabled={loading}>{loading ? "Loading" : "Sign In"}</button>
+                    <input type="password" placeholder='Mot de passe' name='password'/>
+                    <button disabled={loading}>{loading ? "Loading" : "Se connecter"}</button>
                 </form>
             </div>
             <div className="separator"></div>
             <div className="item">
-            <h2>Create an Account</h2>
+            <h2>Créer un compte</h2>
                 <form onSubmit={handleRegister}>
                     <label htmlFor="file">
                         <img src={avatar.url || "./avatar.png"} alt="" />
-                        Upload an image</label>
+                        Téléverser une image</label>
                     <input type="file" id='file' style={{display:"none"}} onChange={handleAvatar}/>
-                    <input type="text" placeholder='Username' name='username'/>
+                    <input type="text" placeholder='Nom d&apos;utilisateur' name='username'/>
                     <input type="text" placeholder='Email' name='email'/>
-                    <input type="password" placeholder='Password' name='password'/>
-                    <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
+                    <input type="password" placeholder='Mot de passe' name='password'/>
+                    <button disabled={loading}>{loading ? "Loading" : "S'inscrire"}</button>
                 </form>
             </div>
         </div>
